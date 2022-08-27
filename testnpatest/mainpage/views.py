d3f894dff069a47fc39c2430398a46b63acfae44
Glazerrr <glazerrr228@gmail.com>
2022-08-27 19:09:54 +0300

    temp

--- testnpatest/mainpage/views.py
+++ testnpatest/mainpage/views.py
@@ -6,7 +6,7 @@ import mainpage.py.nlp_model as nlp_m
 
 # Create your views here.
 m = 0
-a = 1
+a = 0
 def index(request):
     nlp_m.analyze_doc()
     return render(request, 'mainpage/index.html', {'a': a})
