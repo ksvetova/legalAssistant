efc0394c3fd0ed9ac91abbb4c1d6aa4630fe9b9f
Glazerrr <glazerrr228@gmail.com>
2022-08-27 22:14:14 +0300

    a

--- testnpatest/mainpage/views.py
+++ testnpatest/mainpage/views.py
@@ -2,13 +2,13 @@
 from django.shortcuts import render
 import time
 from django.http import *
-import mainpage.py.nlp_model as nlp_m
+# import nlp_model as nlp_m
 
 # Create your views here.
 m = 0
 a = 0
 def index(request):
-    nlp_m.analyze_doc()
+    # nlp_m.analyze_doc()
     return render(request, 'mainpage/index.html', {'a': a})
 
 def tags(request):
