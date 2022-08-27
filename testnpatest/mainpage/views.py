589e2152f290f5afd7adb7a24a00f26f6b9411c4
Dzigen <menshikov.mikhail.2001@gmail.com>
2022-08-27 19:02:23 +0300

    start model connection

--- testnpatest/mainpage/views.py
+++ testnpatest/mainpage/views.py
@@ -2,11 +2,13 @@
 from django.shortcuts import render
 import time
 from django.http import *
+import mainpage.py.nlp_model as nlp_m
 
 # Create your views here.
 m = 0
 a = 1
 def index(request):
+    nlp_m.analyze_doc()
     return render(request, 'mainpage/index.html', {'a': a})
 
 def tags(request):
