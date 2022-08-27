86426a907c9f3f5c7e9de660d80c8958213a66f0
Dzigen <menshikov.mikhail.2001@gmail.com>
2022-08-28 01:23:51 +0300

    start venv

---
+++ testnpatest/mainpage/nlp_model/utils.py
@@ -0,0 +1,22 @@
+#import pandas as pd
+#import json
+#from transformers import AutoTokenizer, AutoModel
+#import torch
+#from collections import Counter
+#import math
+#from sklearn.model_selection import train_test_split
+#from catboost import CatBoostClassifier, Pool
+#from IPython.display import clear_output
+#import numpy as np
+#from itertools import groupby
+#import ast
+#import nltk
+#from nltk.tokenize import sent_tokenize, word_tokenize
+#import docx2txt
+#import re
+
+#nltk.download('punkt')
+
+
+def test_function():
+    print("Hello World!")
